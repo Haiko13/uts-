c645da6c4080af3d772724fbd270c1850f0b48b1
Haiko13 <haikobeater@gmail.com>
2022-12-03 13:37:25 +0800

    first commit

---
+++ resources/views/komponen.blade.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title> SKEMA </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    <div class="container">
+        <!-- navbar start -->
+        <div class="div-navbar">
+            <ul class="ul-navbar">
+                <li class="li-navbar">
+                    <a href="#" class="background-navbar">HOME</a>
+                </li>
+                <li class="li-navbar">
+                    <a href="/skema" class="background-navbar">SKEMA</a>
+                </li>
+                <li class="li-navbar">
+                    <a href="/komponen" class="background-navbar">KOMPONEN</a>
+                </li>
+            </ul>
+        </div>
+        <!-- content komponen start -->
+        <div class="komponen">KOMPONEN YANG DIPAKAI<br>1. ARDUINO UNO<br>2.  BIG SOUND SENSOR<br>3.  LIGHT BULB<br>4.   5V RELAY<br>
+            5.  STAKER<br>6.    BREADBOARD<br>7.  JUMPER WIRE<br>8.TERMINAL TAKER  <br>9.ELECTRIC WIRE    
+        
+        </div>    
+        <!-- content komponen end -->
+    </body>
+</html>
---
+++ resources/views/skema.blade.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title> SKEMA </title>
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    <div class="container">
+        <!-- navbar start -->
+        <div class="div-navbar">
+            <ul class="ul-navbar">
+                <li class="li-navbar">
+                    <a href="#" class="background-navbar">HOME</a>
+                </li>
+                <li class="li-navbar">
+                    <a href="/skema" class="background-navbar">SKEMA</a>
+                </li>
+                <li class="li-navbar">
+                    <a href="/komponen" class="background-navbar">KOMPONEN</a>
+                </li>
+            </ul>
+        </div>
+        <!-- content skema start -->
+        <j class="text-skema">INI ADALAH SKEMA NYA</j>
+        <div class="container-skema">
+            <img src="img/Skema UTS.png" class="img-skema"/>       
+        </div>
+        <!-- content 1 end -->
+            </body>
+</html>
